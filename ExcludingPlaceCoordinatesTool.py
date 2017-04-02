c24c47b894d5f95543b193712a09135eb5d64606
Unknown <encappozzo@gmail.com>
2017-04-02 17:28:26 +0200

    User interaction

--- ExcludingPlaceCoordinatesTool.py
+++ ExcludingPlaceCoordinatesTool.py
@@ -1,32 +1,53 @@
 
-#EX. waldo_face = (133,480),(133,481),(133,482),(133,483)
+import rlcompleter 
 
 def deleteContent(file):
     file.seek(0)
     file.truncate()
+    
+def getUsrInput():
+    print ("Insert X coordinate of the column you want to exclude:")
+    colX=int(input())
+    print ("Insert Y min:")
+    fromY=int(input())
+    print ("Insert Y max:")
+    toY=int(input())
+
+    print("Coordinates:\n X--> "+ str(colX) + "\nY--> From Y= "+str(fromY)+" to Y= "+str(toY))
+    print("Confirm? Y/N")
+    usrChoice=input()
+    
+    if usrChoice.lower() == "y":
+        return [colX,fromY,toY]
+    else: 
+        getUsrInput()
+
+def columnToExclude(coord):
+    for y in range(coord[1],coord[2],1):
+        textFile.write("("+ str(coord[0]) + "," +  str(y) + "),")
+
+
+        
+#EX--> drawingName = [(colX,fromY),(133,481),(133,482),...(133,509),(colX,toY)]
 
 fileToWriteName= "coordToExclude.txt"
-drawingToExcludeName = "drawName"
-fromX = 133
-toX=134
-fromY=480
-toY=509
+drawingToExcludeName = "drawingName"
+fromY=0
+toY=0
+colX =0
+        
 
 print ("Open file: " + fileToWriteName)
 textFile = open(fileToWriteName, "a")
-
-print ("Deleting previous file content")
+print ("Deleting previous file content...")
 deleteContent(textFile)
 
-print ("Write to file...")
 textFile.write(drawingToExcludeName + " = [")
 
-for x in range(fromX,toX+1,1):
-    for y in range(fromY,toY,1):
-        textFile.write("("+ str(x) + "," +  str(y) + "),")
-    #textFile.write('\n')
-        
-textFile.write("("+ str(toX) + "," +  str(toY) + ")]")
-
+coordToExclude = getUsrInput()
+columnToExclude(coordToExclude)
+textFile.write("("+ str(coordToExclude[0]) + "," +  str(coordToExclude[2]) + ")]")
 textFile.close()
+
+
 print ("Done.")
